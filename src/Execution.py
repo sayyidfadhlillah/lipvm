789ae9725c6135b04766d3a4e21e74588b3d9177
Hafiyyan Sayyid Fadhlillah <hafiyyan94@gmail.com>
2026-01-16 17:38:17 +0100

    Improved execution, step forward and backward is dictated by Snapshot instruction instead of bytecode

--- src/Execution.py
+++ src/Execution.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 
 class Execution:
 
-    def __init__(self, bytecode, environment):
+    def __init__(self, bytecode, environment, runtime_mode=True):
         self._history = []
         self._interrupt = False
 
@@ -16,7 +16,12 @@ class Execution:
         self._environment = environment
 
         self._initial_environment = environment
-   
+        self._runtime_mode = runtime_mode
+
+    def is_runtime_mode(self):
+
+        return self._runtime_mode
+
     def started(self):
         return self._environment.ip > 0
 
@@ -27,10 +32,16 @@ class Execution:
         self._history = []
         self._environment = self._initial_environment
 
+    # def start(self):
+    #     while not (self._interrupt or self.ended()):
+    #         self.step_forward()
+    #     self._interrupt = False
+
     def start(self):
+
         while not (self._interrupt or self.ended()):
+
             self.step_forward()
-        self._interrupt = False
 
     def stop(self):
         if self.started():
@@ -38,19 +49,29 @@ class Execution:
         else:
             raise Exception("Trying to stop a non-running execution")
 
+    # Execute instruction in the bytecode one step at a time
+    # Each step is limited by a Snapshot instruction
     def step_forward(self):
+
         if self.ended():
             raise Exception("Cannot step forward an execution that has ended")
 
-        instruction = self._bytecode.instructions[self._environment.ip]
-        if type(instruction) is Snapshot:
-            self._history.append(deepcopy(self._environment))
-        else:
-            instruction.execute(self._environment)
+        instruction_is_a_snapshot: bool = False
 
-        self._environment.ip += 1
+        while not (self._interrupt or self.ended() or instruction_is_a_snapshot):
+
+            instruction = self._bytecode.instructions[self._environment.ip]
+            instruction_is_a_snapshot = type(instruction) is Snapshot
+
+            if instruction_is_a_snapshot:
+                self._history.append(deepcopy(self._environment))
+            else:
+                instruction.execute(self._environment)
+
+            self._environment.ip += 1
 
     def step_backward(self):
+
         if self._environment.ip == 0:
             raise Exception("Cannot step backward beyond the start of an execution")
 
